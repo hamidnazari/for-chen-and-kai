fe8be13caf3db09d72e037632f80d2409e60e6df
Hamid Nazari <hn@linux.com>
2024-09-10 09:44:46 +1000

    Added a simple test case

---
+++ tests/test_connection.py
@@ -0,0 +1,19 @@
+import os
+import pytest
+from src.db.connection import Connection
+
+
+@pytest.fixture
+def db_connection():
+    test_db = 'test_database.db'
+    conn = Connection(test_db)
+    yield conn
+
+
+def test_load_csv(db_connection):
+    test_csv = 'contacts.csv'
+    db_connection.load(test_csv)
+
+    result = db_connection.list()
+
+    assert len(result) == 10_000
